5c9d23513e5f006c965df2169e4c154153527edb
ShengjieMao <56333697+ShengjieMao@users.noreply.github.com>
2023-10-21 16:06:48 -0700

    add view controller

---
+++ StockApp/AddStockViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddStockViewController.swift
+//  StockApp
+//
+//  Created by Shengjie Mao on 10/21/23.
+//
+
+import UIKit
+
+class AddStockViewController: UIViewController {
+
+    @IBOutlet weak var txtStock: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addStock(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
