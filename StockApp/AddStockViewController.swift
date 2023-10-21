6ab94254786e703dc4429b1b2abd939f641eee48
ShengjieMao <56333697+ShengjieMao@users.noreply.github.com>
2023-10-21 16:56:05 -0700

    api

--- StockApp/AddStockViewController.swift
+++ StockApp/AddStockViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftUI
+import SwiftyJSON
 
 class AddStockViewController: UIViewController {
 
@@ -17,7 +20,43 @@ class AddStockViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func addStock(_ sender: Any) {
+    @IBAction func addStockAction(_ sender: Any) {
+        guard let stockSymbol = txtStock.text else {return}
+        let url = "\(baseURL)profile/\(stockSymbol)?apikey=\(apiKey)"
+
+        AF.request(url).responseJSON { response in
+
+            if(response.error != nil){
+                print(response.error?.localizedDescription)
+                return
+            }
+
+            // We have valid data here
+            guard let rawData = response.data else {return}
+            
+            guard let jsonArray = JSON(rawData).array else {return}
+
+            guard let stock = jsonArray.first else {return}
+
+            let symbol = stock["symbol"].stringValue
+
+            let price = stock["price"].floatValue
+
+            let companyName = stock["companyName"].stringValue
+
+            let description = stock["description"].stringValue
+
+            print(symbol)
+            print(price)
+            print(companyName)
+            print(description)
+            // let stockClass = StockClass()
+            // stockClass.symbol = symbol
+            // stockClass.price = price
+            // stockClass.companyName = companyName
+            // stockClass.desc = description
+            // self.addStockToDB(stockClass)
+        }
     }
     
     /*
---
+++ StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Shengjie Mao on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "b77ab5fd66c520edcbf1171660c8ae04"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
